b33628357d3e5f5cd432134350d75a6fabd5dcfa
FlackAP <flack.ap@gmail.com>
2014-03-28 14:35:02 -0400

    added prototype cell

--- TaskMaster2/APFMainViewController.m
+++ TaskMaster2/APFMainViewController.m
@@ -26,4 +26,14 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
+    
+    return cell;
+}
+
 @end
