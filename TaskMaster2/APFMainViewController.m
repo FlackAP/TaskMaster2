b318c350570ea1491519a1d9927d49c3d888a51b
FlackAP <flack.ap@gmail.com>
2014-03-28 15:20:01 -0400

    added rows to table view

--- TaskMaster2/APFMainViewController.m
+++ TaskMaster2/APFMainViewController.m
@@ -27,11 +27,25 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 5;
+    return 100;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
+    
+    UILabel *label = (UILabel *)[cell viewWithTag:1000];
+    if (indexPath.row % 5 == 0) {
+        label.text = @"Walk the dog";
+    } else if (indexPath.row % 5 == 1) {
+        label.text = @"Drink a beer";
+    } else if (indexPath.row % 5 == 2) {
+        label.text = @"Learn iOS development";
+    } else if (indexPath.row % 5 == 3) {
+        label.text = @"Drink a beer";
+    } else if (indexPath.row % 5 == 4) {
+        label.text = @"Eat ice cream";
+    }
     
     return cell;
 }
