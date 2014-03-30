bc8e791267d932305a3ef5352b8ceafc431e3d95
FlackAP <flack.ap@gmail.com>
2014-03-30 15:15:33 -0400

    determined whether row is checked or not

--- TaskMaster2/APFMainViewController.m
+++ TaskMaster2/APFMainViewController.m
@@ -13,11 +13,29 @@
 @end
 
 @implementation APFMainViewController
+{
+    NSString *_row0text;
+    NSString *_row1text;
+    NSString *_row2text;
+    NSString *_row3text;
+    NSString *_row4text;
+    
+    BOOL _row0checked;
+    BOOL _row1checked;
+    BOOL _row2checked;
+    BOOL _row3checked;
+    BOOL _row4checked;
+}
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    _row0text = @"Walk the dog";
+    _row1text = @"Brush teeth";
+    _row2text = @"Learn iOS development";
+    _row3text = @"Soccer practice";
+    _row4text = @"Eat ice cream";
 }
 
 - (void)didReceiveMemoryWarning
@@ -27,7 +45,7 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 100;
+    return 5;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -35,16 +53,16 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
     
     UILabel *label = (UILabel *)[cell viewWithTag:1000];
-    if (indexPath.row % 5 == 0) {
-        label.text = @"Walk the dog";
-    } else if (indexPath.row % 5 == 1) {
-        label.text = @"Drink a beer";
-    } else if (indexPath.row % 5 == 2) {
-        label.text = @"Learn iOS development";
-    } else if (indexPath.row % 5 == 3) {
-        label.text = @"Drink a beer";
-    } else if (indexPath.row % 5 == 4) {
-        label.text = @"Eat ice cream";
+    if (indexPath.row == 0) {
+        label.text = _row0text;
+    } else if (indexPath.row == 1) {
+        label.text = _row1text;
+    } else if (indexPath.row == 2) {
+        label.text = _row2text;
+    } else if (indexPath.row == 3) {
+        label.text = _row3text;
+    } else if (indexPath.row == 4) {
+        label.text = _row4text;
     }
     
     return cell;
@@ -53,13 +71,32 @@
 - (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     
-    if (cell.accessoryType == UITableViewCellAccessoryNone) {
-        cell.accessoryType = UITableViewCellAccessoryCheckmark;
-    } else {
+    BOOL isChecked = NO;
+    if (indexPath.row == 0) {
+        isChecked = _row0checked;
+        _row0checked = !_row0checked;
+    } else if (indexPath.row == 1) {
+        isChecked = _row1checked;
+        _row1checked = !_row1checked;
+    } else if (indexPath.row == 2) {
+        isChecked = _row2checked;
+        _row2checked = !_row2checked;
+    } else if (indexPath.row == 3) {
+        isChecked = _row3checked;
+        _row3checked = !_row3checked;
+    } else if (indexPath.row == 4) {
+        isChecked = _row4checked;
+        _row4checked = !_row4checked;
+    }
+    if (isChecked) {
         cell.accessoryType = UITableViewCellAccessoryNone;
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
     }
     
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
 }
+
 
 @end
