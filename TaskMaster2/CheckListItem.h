d6c287cc48c736a72ae177f400592bc0e21413d6
FlackAP <flack.ap@gmail.com>
2014-03-31 08:53:13 -0400

    created Checklist Item object and created instances for each row

---
+++ TaskMaster2/CheckListItem.h
@@ -0,0 +1,16 @@
+//
+//  CheckListItem.h
+//  TaskMaster2
+//
+//  Created by Flack AP on 3/31/14.
+//  Copyright (c) 2014 Flack AP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CheckListItem : NSObject
+
+@property (nonatomic, copy) NSString *text;
+@property (nonatomic, assign) BOOL checked;
+
+@end
---
+++ TaskMaster2/CheckListItem.m
@@ -0,0 +1,13 @@
+//
+//  CheckListItem.m
+//  TaskMaster2
+//
+//  Created by Flack AP on 3/31/14.
+//  Copyright (c) 2014 Flack AP. All rights reserved.
+//
+
+#import "CheckListItem.h"
+
+@implementation CheckListItem
+
+@end
